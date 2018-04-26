369002216c061de99e88a2e7edde84a1da23c9d1
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2018-04-26 20:29:06 +0600

    Add files via upload

---
+++ CST_117_Exercise_1_EWilson/CST_117_Exercise_1_EWilson/Exercises1and2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace CST_117_Exercise_1_EWilson
+{
+    partial class Exercises1And2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createAccountLabel = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // createAccountLabel
+            // 
+            this.createAccountLabel.AutoSize = true;
+            this.createAccountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.createAccountLabel.Location = new System.Drawing.Point(42, 86);
+            this.createAccountLabel.Name = "createAccountLabel";
+            this.createAccountLabel.Size = new System.Drawing.Size(767, 91);
+            this.createAccountLabel.TabIndex = 0;
+            this.createAccountLabel.Text = "Create New Account";
+            this.createAccountLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.userLabel.Location = new System.Drawing.Point(48, 251);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(281, 55);
+            this.userLabel.TabIndex = 1;
+            this.userLabel.Text = "User Name:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.passwordLabel.Location = new System.Drawing.Point(48, 351);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(249, 55);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.submitButton.Location = new System.Drawing.Point(258, 467);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(372, 134);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nameTextBox.Location = new System.Drawing.Point(335, 244);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(505, 62);
+            this.nameTextBox.TabIndex = 4;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.passwordTextBox.Location = new System.Drawing.Point(335, 344);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(505, 62);
+            this.passwordTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 693);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.createAccountLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "New User";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label createAccountLabel;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
---
+++ CST_117_Exercise_1_EWilson/CST_117_Exercise_1_EWilson/Exercises1and2.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * Program: Exercise 1 & 2
+ * File: Exercises_1-2.cs
+ * Summary: Basic windows form with controls and minimal function
+ * Author: Evan Wilson
+ * Date: April 26th, 2018
+ **/
+
+using System;
+using System.Windows.Forms;
+
+namespace CST_117_Exercise_1_EWilson
+{
+    public partial class Exercises1And2 : Form
+    {
+        public Exercises1And2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if (nameTextBox.Text.Length >= 4 && passwordTextBox.Text.Length >= 6)
+            {
+                MessageBox.Show("Account Successfully Created \r\n \r\n" +
+                        "User Name: " + nameTextBox.Text +
+                        "\r\nPassword: " + passwordTextBox.Text, "Success");
+            }
+            else
+            {
+                if (nameTextBox.Text.Length < 4 && passwordTextBox.Text.Length >= 6)
+                {
+                    MessageBox.Show("User name must be at least 4 characters long!", "Error");
+                }
+
+                if (passwordTextBox.Text.Length < 6 && nameTextBox.Text.Length >= 4)
+                {
+                    MessageBox.Show("Password must be at least 6 characters long!", "Error");
+                }
+
+                if (nameTextBox.Text.Length < 4 && passwordTextBox.Text.Length < 6)
+                {
+                    MessageBox.Show("You must enter a user name and password \r\n" +
+                                    "User name must be at least 4 characters long! \r\n" +
+                                    "Password must be at least 6 characters long!", "Error");
+                }
+            }
+        }
+    }
+}
