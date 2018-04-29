e80b5dd9b4e3e9d7a624b0dfa35d348d05ac0927
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2018-04-29 20:34:53 +0600

    Add files via upload

---
+++ Week1_Exercise3/Week1_Exercise3/Week1Exercise3.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Week1_Exercise3
+{
+    partial class Week1Exercise3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Week1Exercise3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.fahrenheitRadioButton = new System.Windows.Forms.RadioButton();
+            this.celsiusRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tempLabel = new System.Windows.Forms.Label();
+            this.convertedTempLabel = new System.Windows.Forms.Label();
+            this.tempTextBox = new System.Windows.Forms.TextBox();
+            this.convertedTempTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label1.Location = new System.Drawing.Point(31, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1172, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Temperature Unit:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // fahrenheitRadioButton
+            // 
+            this.fahrenheitRadioButton.AutoSize = true;
+            this.fahrenheitRadioButton.Checked = true;
+            this.fahrenheitRadioButton.Location = new System.Drawing.Point(52, 86);
+            this.fahrenheitRadioButton.Name = "fahrenheitRadioButton";
+            this.fahrenheitRadioButton.Size = new System.Drawing.Size(384, 77);
+            this.fahrenheitRadioButton.TabIndex = 1;
+            this.fahrenheitRadioButton.TabStop = true;
+            this.fahrenheitRadioButton.Text = "Fahrenheit";
+            this.fahrenheitRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // celsiusRadioButton
+            // 
+            this.celsiusRadioButton.AutoSize = true;
+            this.celsiusRadioButton.Location = new System.Drawing.Point(52, 168);
+            this.celsiusRadioButton.Name = "celsiusRadioButton";
+            this.celsiusRadioButton.Size = new System.Drawing.Size(286, 77);
+            this.celsiusRadioButton.TabIndex = 2;
+            this.celsiusRadioButton.TabStop = true;
+            this.celsiusRadioButton.Text = "Celsius";
+            this.celsiusRadioButton.UseVisualStyleBackColor = true;
+            this.celsiusRadioButton.CheckedChanged += new System.EventHandler(this.celsiusRadioButton_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fahrenheitRadioButton);
+            this.groupBox1.Controls.Add(this.celsiusRadioButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.groupBox1.Location = new System.Drawing.Point(368, 231);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(509, 318);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.AutoSize = true;
+            this.tempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.tempLabel.Location = new System.Drawing.Point(33, 645);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(418, 73);
+            this.tempLabel.TabIndex = 4;
+            this.tempLabel.Text = "Temperature:";
+            // 
+            // convertedTempLabel
+            // 
+            this.convertedTempLabel.AutoSize = true;
+            this.convertedTempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.convertedTempLabel.Location = new System.Drawing.Point(46, 805);
+            this.convertedTempLabel.Name = "convertedTempLabel";
+            this.convertedTempLabel.Size = new System.Drawing.Size(732, 73);
+            this.convertedTempLabel.TabIndex = 5;
+            this.convertedTempLabel.Text = "Converted Temperature:";
+            // 
+            // tempTextBox
+            // 
+            this.tempTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.tempTextBox.Location = new System.Drawing.Point(784, 628);
+            this.tempTextBox.Name = "tempTextBox";
+            this.tempTextBox.Size = new System.Drawing.Size(321, 80);
+            this.tempTextBox.TabIndex = 6;
+            this.tempTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tempTextBox.TextChanged += new System.EventHandler(this.tempTextBox_TextChanged);
+            // 
+            // convertedTempTextBox
+            // 
+            this.convertedTempTextBox.Enabled = false;
+            this.convertedTempTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.convertedTempTextBox.Location = new System.Drawing.Point(784, 798);
+            this.convertedTempTextBox.Name = "convertedTempTextBox";
+            this.convertedTempTextBox.Size = new System.Drawing.Size(321, 80);
+            this.convertedTempTextBox.TabIndex = 7;
+            this.convertedTempTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1133, 638);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1133, 798);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(86, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1133, 798);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(86, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(1133, 638);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(86, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // Week1Exercise3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(288F, 288F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1245, 1009);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.convertedTempTextBox);
+            this.Controls.Add(this.tempTextBox);
+            this.Controls.Add(this.convertedTempLabel);
+            this.Controls.Add(this.tempLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Week1Exercise3";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 50);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Week 1 Exercise 3: Temperature Conversion";
+            this.Load += new System.EventHandler(this.Week1Exercise3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton fahrenheitRadioButton;
+        private System.Windows.Forms.RadioButton celsiusRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label tempLabel;
+        private System.Windows.Forms.Label convertedTempLabel;
+        private System.Windows.Forms.TextBox tempTextBox;
+        private System.Windows.Forms.TextBox convertedTempTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Week1_Exercise3/Week1_Exercise3/Week1Exercise3.cs
@@ -0,0 +1,82 @@
+﻿/**
+ * Program: Exercise 3 & Project 1
+ * File: Week1Exercise3.cs
+ * Summary: Fahrenheit/Celsius temperature conversion program
+ * Author: Evan Wilson
+ * Date: April 26th, 2018
+ **/
+
+using System;
+using System.Windows.Forms;
+
+namespace Week1_Exercise3
+{
+    public partial class Week1Exercise3 : Form
+    {
+        public Week1Exercise3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void Week1Exercise3_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void tempTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (tempTextBox.Text == "")
+            {
+                convertedTempTextBox.Text = "";
+            }
+            else
+            {
+                try
+                {
+                    decimal originalTemp = decimal.Parse(tempTextBox.Text);
+                    if (fahrenheitRadioButton.Checked)
+                    {
+                        decimal convertedTemp = (originalTemp - 32) * 5 / 9;
+                        convertedTempTextBox.Text = Math.Round(convertedTemp, 3).ToString();
+                    }
+                    else if (celsiusRadioButton.Checked)
+                    {
+                        decimal convertedTemp = originalTemp * 9 / 5 + 32;
+                        convertedTempTextBox.Text = Math.Round(convertedTemp, 3).ToString();
+                    }
+                }
+                catch
+                {
+                    tempTextBox.Text = "";
+                    MessageBox.Show("You can only enter numbers!");
+                }
+            }
+        }
+
+        private void celsiusRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            tempTextBox.Text = "";
+            if (fahrenheitRadioButton.Checked)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = false;
+                pictureBox4.Visible = false;
+            }
+            else if (celsiusRadioButton.Checked)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = true;
+                pictureBox4.Visible = true;
+            }
+        }
+    }
+}
